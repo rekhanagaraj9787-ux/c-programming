0b77fde4e952e85c703357916ca3db3162c9b10d
rekhanagaraj9787-ux <rekhanagaraj9787@gmail.com>
2026-02-18 09:50:05 +0530

    Add files via upload

---
+++ conditional statement/c program2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int mark;
+    printf(" enter the mark: ");
+    
+    scanf("%d",&mark);
+    if (mark>=40)
+    printf("pass");
+    else
+    printf("fail");
+    
+}
